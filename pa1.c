f0ce2e83f46bf0072d0b8ec1c183094ae3bb9291
Sang-Hoon Kim <sanghoonkim@ajou.ac.kr>
2019-09-24 13:25:51 +0900

    Revise template code

--- pa1.c
+++ pa1.c
@@ -16,31 +16,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
+#include <getopt.h>
 
 #include "types.h"
 #include "parser.h"
 
-static char prompt[MAX_TOKEN_LEN] = "$";
+/*====================================================================*/
+/*          ****** DO NOT MODIFY ANYTHING FROM THIS LINE ******       */
+/**
+ * String used as the prompt (see @main()). You may change this to
+ * change the prompt */
+static char __prompt[MAX_TOKEN_LEN] = "$";
+
+/**
+ * Time out value. It's OK to read this value, but ** DO NOT CHANGE
+ * DIRECTLY **. Instead, use @set_timeout() function below.
+ */
+static unsigned int __timeout = 2;
+
+static void set_timeout(unsigned int timeout)
+{
+	__timeout = timeout;
+
+	if (__timeout == 0) {
+		fprintf(stderr, "Timeout is disabled\n");
+	} else {
+		fprintf(stderr, "Timeout is set to %d second%s\n",
+				__timeout,
+				__timeout >= 2 ? "s" : "");
+	}
+}
+/*          ****** DO NOT MODIFY ANYTHING UP TO THIS LINE ******      */
+/*====================================================================*/
 
 
 /***********************************************************************
  * run_command()
  *
  * DESCRIPTION
- *   Implement the specified shell features here using the parsed command
- *   tokens.
+ *   Implement the specified shell features here using the parsed
+ *   command tokens.
  *
  * RETURN VALUE
  *   Return 1 on successful command execution
- *   Return 0 when user inputs "exit".
+ *   Return 0 when user inputs "exit"
  *   Return <0 on error
  */
 static int run_command(int nr_tokens, char *tokens[])
 {
 	/* This function is all yours. Good luck! */
-
-	assert(nr_tokens > 0);
 
 	if (strncmp(tokens[0], "exit", strlen("exit")) == 0) {
 		return 0;
@@ -57,16 +81,67 @@ static int run_command(int nr_tokens, char *tokens[])
 
 
 /***********************************************************************
- * main() of this program.
+ * initialize()
  *
- * *** DO NOT MODIFY ANYTHING BELOW THIS LINE ***
+ * DESCRIPTION
+ *   Call-back function for your own initialization code. It is OK to
+ *   leave blank if you don't need any initialization.
+ *
+ * RETURN VALUE
+ *   Return 0 on successful initialization.
+ *   Return other value on error, which leads the program to exit.
  */
-int main(int argc, const char *argv[])
+static int initialize(int argc, char * const argv[])
+{
+	return 0;
+}
+
+
+/***********************************************************************
+ * finalize()
+ *
+ * DESCRIPTION
+ *   Callback function for finalizing your code. Like @initialize(),
+ *   you may leave this function blank.
+ */
+static void finalize(int argc, char * const argv[])
+{
+
+}
+
+
+/*====================================================================*/
+/*          ****** DO NOT MODIFY ANYTHING BELOW THIS LINE ******      */
+
+static bool __verbose = true;
+static char *__color_start = "[0;31;40m";
+static char *__color_end = "[0m";
+
+/***********************************************************************
+ * main() of this program.
+ */
+int main(int argc, char * const argv[])
 {
 	char command[MAX_COMMAND_LEN] = { '\0' };
 	int ret = 0;
+	int opt;
 
-	printf("[0;31;40m%s[0m ", prompt);
+	while ((opt = getopt(argc, argv, "qm")) != -1) {
+		switch (opt) {
+		case 'q':
+			__verbose = false;
+			break;
+		case 'm':
+			__color_start = __color_end = "\0";
+			break;
+		}
+	}
+
+	if ((ret = initialize(argc, argv))) return EXIT_FAILURE;
+
+	if (__verbose)
+		fprintf(stderr, "%s%s%s ", __color_start, __prompt, __color_end);
+
 	while (fgets(command, sizeof(command), stdin)) {	
 		char *tokens[MAX_NR_TOKENS] = { NULL };
 		int nr_tokens = 0;
@@ -82,8 +157,11 @@ int main(int argc, const char *argv[])
 		}
 
 more:
-		printf("[0;31;40m%s[0m ", prompt);
+		if (__verbose)
+			fprintf(stderr, "%s%s%s ", __color_start, __prompt, __color_end);
 	}
 
-	return ret;
+	finalize(argc, argv);
+
+	return EXIT_SUCCESS;
 }
