8b342189c74a9a65d5d56458f700477955adb850
terajh95 <terajoohyun@ajou.ac.kr>
2019-10-05 18:51:05 +0900

    19_10_5_final

--- pa1.c
+++ pa1.c
@@ -203,7 +203,7 @@ int main(int argc, char * const argv[])
 	if ((ret = initialize(argc, argv))) return EXIT_FAILURE;
 
 	if (__verbose)
-		fprintf(stderr, "%s%s%s%s ", __color_start, "FuckU",__prompt, __color_end);
+		fprintf(stderr, "%s%s%s ", __color_start,__prompt, __color_end);
 
 	while (fgets(command, sizeof(command), stdin)) {	
 		char *tokens[MAX_NR_TOKENS] = { NULL };
@@ -222,7 +222,7 @@ int main(int argc, char * const argv[])
 
 more:
 		if (__verbose)
-			fprintf(stderr, "%s%s%s%s ", __color_start,"FuckU", __prompt, __color_end);
+			fprintf(stderr, "%s%s%s ", __color_start, __prompt, __color_end);
 	}
 
 	finalize(argc, argv);
